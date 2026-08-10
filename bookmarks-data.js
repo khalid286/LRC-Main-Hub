ec32acb47ed357cdb4f067c834fb65c319d9750f
khalid286 <w2092006@apps.losrios.edu>
2026-08-10 15:17:41 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- bookmarks-data.js
+++ bookmarks-data.js
@@ -1,18 +1,18 @@
 window.BOOKMARKS = [
   {
-    "title": "Arc",
+    "title": "ARC Home Page",
     "url": "https://arc.losrios.edu/",
     "category": "Quick Links",
     "domain": "arc.losrios.edu"
   },
   {
-    "title": "Inside",
+    "title": "ARC Employee",
     "url": "https://inside.arc.losrios.edu/",
     "category": "Quick Links",
     "domain": "inside.arc.losrios.edu"
   },
   {
-    "title": "Us06Web",
+    "title": "Zoom",
     "url": "https://us06web.zoom.us/meeting#/upcoming",
     "category": "Quick Links",
     "domain": "us06web.zoom.us"
@@ -24,195 +24,87 @@ window.BOOKMARKS = [
     "domain": "calendar.google.com"
   },
   {
-    "title": "Dept Agenda",
-    "url": "https://docs.google.com/document/d/1uLzPo15nmJBivvJZLIK_EGa9Ps7kiIn-IMw0FNVLrpc/edit?pli=1&tab=t.0",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "🎓 Tutoring Outreach Plan – Semester-Based Calendar - Google Docs",
-    "url": "https://docs.google.com/document/d/1WCgsVhjd6R6cvJvUFoQOFL4ZHqAWdqDdhdju1wYj7x4/edit?tab=t.0#heading=h.ms3zu03gk1ur",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "LRC Department Meeting Agenda - Google Docs",
-    "url": "https://docs.google.com/document/d/1uLzPo15nmJBivvJZLIK_EGa9Ps7kiIn-IMw0FNVLrpc/edit?tab=t.0",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "LRC Tutoring/SI Budget Meetings - Google Docs",
-    "url": "https://docs.google.com/document/d/1H7Ntu4QhZegC4Po6xw1Y_lnJn6BInMz7yJosEQsAAxk/edit?tab=t.0",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "25/26 Tutoring Budgets",
-    "url": "https://docs.google.com/spreadsheets/d/1UUC4XoWf22aKZHdhl-WWCAIozyCj9fPSqsKBOfqwySw/edit?gid=0#gid=0",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "Student",
-    "url": "https://ps.losrios.edu/student/signon.html",
-    "category": "Meetings",
-    "domain": "ps.losrios.edu"
-  },
-  {
-    "title": "Beacon Meeting Agenda - Google Docs",
-    "url": "https://docs.google.com/document/d/1GKw0YAkgw07rixdBH12tfDCwoRVD90HnOUBkMJLehOI/edit?tab=t.5bswyqcqe460",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "LRC Summer 2026 Offerings - Google Docs",
-    "url": "https://docs.google.com/document/d/1nXEgMT60MAOYXPQXNpvX6KryIIAvMb3zk4BuYNxI3Rc/edit?tab=t.hq7dnlggwjzg",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "Summer 2026 Hiring - Google Sheets",
-    "url": "https://docs.google.com/spreadsheets/d/19pT4flS6cnLf5GHbWHY09lgywGqHCIWyS8eLoOKdWcA/edit?gid=160270350#gid=160270350",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "LRC Summer 2026 Schedule - Google Sheets",
-    "url": "https://docs.google.com/spreadsheets/d/1Z2w5Zf70HaSojJeCHLIrl1fBQyFxdlKjyWRlHakHlQM/edit?gid=549755767#gid=549755767",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "2026-27 LRC Budgets - Google Sheets",
-    "url": "https://docs.google.com/spreadsheets/d/1TXl3EQm7EUS6_3gAu3_LvFOHFiC9htDjBCoG5jCA8Gs/edit?gid=0#gid=0",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "Copy of 2026-27 LRC Budgets - Google Sheets",
-    "url": "https://docs.google.com/spreadsheets/d/113b_qX6s-Jo8jM-L83erDnG3t2As7Cxn3ZfV20lI1y0/edit?gid=0#gid=0",
-    "category": "Meetings",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "LRC",
+    "title": "LRC Penji",
     "url": "https://admin.penjiapp.com/communities/ABoPC5axwq/data-dashboard",
-    "category": "G/E",
+    "category": "General Tutoring",
     "domain": "admin.penjiapp.com"
   },
   {
-    "title": "LRC",
+    "title": "LRC Drive",
     "url": "https://drive.google.com/drive/folders/0AMc_TOWQm2sJUk9PVA",
-    "category": "G/E",
+    "category": "General Tutoring",
     "domain": "drive.google.com"
-  },
-  {
-    "title": "Summer 2026 Hiring - Google Sheets",
-    "url": "https://docs.google.com/spreadsheets/d/19pT4flS6cnLf5GHbWHY09lgywGqHCIWyS8eLoOKdWcA/edit?gid=1810644778#gid=1810644778",
-    "category": "G/E",
-    "domain": "docs.google.com"
   },
   {
     "title": "LRC Student Help Check-In",
     "url": "https://studhelptrack.arc.losrios.edu/lrc/check.asp",
-    "category": "G/E",
+    "category": "General Tutoring",
     "domain": "studhelptrack.arc.losrios.edu"
   },
   {
     "title": "LRC Student Help Administration",
     "url": "https://studhelptrack.arc.losrios.edu/lrc/admin/",
-    "category": "G/E",
+    "category": "General Tutoring",
     "domain": "studhelptrack.arc.losrios.edu"
   },
   {
     "title": "Fall 2026 Hiring - Google Sheets",
     "url": "https://docs.google.com/spreadsheets/d/1fB0d36rrhE2gPOc4D9kYWYID6EmDMTPzfz0cWXjK9yE/edit?gid=0#gid=0",
-    "category": "G/E",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "Spring 2026 Tutoring Schedule - Google Docs",
-    "url": "https://docs.google.com/document/d/1zynXoUQSG1nFmaeOQUGOmq2EBVqfj4DNzOCZzkFFxiA/edit?tab=t.0",
-    "category": "G/E",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "Summer 2026 Hiring - Google Sheets",
-    "url": "https://docs.google.com/spreadsheets/d/19pT4flS6cnLf5GHbWHY09lgywGqHCIWyS8eLoOKdWcA/edit?gid=0#gid=0",
-    "category": "G/E",
+    "category": "General Tutoring",
     "domain": "docs.google.com"
   },
   {
     "title": "Canva",
     "url": "https://www.canva.com/",
-    "category": "G/E",
+    "category": "General Tutoring",
     "domain": "canva.com"
   },
   {
     "title": "LRC STU Front Desks Work Schedule_SPR26.xlsx",
     "url": "https://losrios-my.sharepoint.com/:x:/r/personal/w1917589_scc_losrios_edu/_layouts/15/Doc.aspx?sourcedoc=%7BDC547203-D404-4A1D-B0B1-4F992875F7BD%7D&file=LRC%20STU%20Front%20Desks%20Work%20Schedule_SPR26.xlsx&action=default&mobileredirect=true",
-    "category": "G/E",
+    "category": "General Tutoring",
     "domain": "losrios-my.sharepoint.com"
   },
   {
-    "title": "Beacon",
+    "title": "Beacon Drive",
     "url": "https://drive.google.com/drive/folders/0AFOao-d5dJO-Uk9PVA",
     "category": "Quick Links",
     "domain": "drive.google.com"
   },
   {
-    "title": "Beacon",
+    "title": "Beacon Penji",
     "url": "https://admin.penjiapp.com/communities/xWnYFZQoYU/data-dashboard",
     "category": "Quick Links",
     "domain": "admin.penjiapp.com"
   },
   {
-    "title": "Spring 26",
-    "url": "https://docs.google.com/spreadsheets/d/1U8aVvlk3QrF2NBlApJFtrKGuVWpfZrp3wXc_hm4Mu7E/edit?gid=0#gid=0",
+    "title": "LRC Drive",
+    "url": "https://drive.google.com/drive/folders/0AMc_TOWQm2sJUk9PVA",
+    "category": "Quick Links",
+    "domain": "drive.google.com"
+  },
+  {
+    "title": "LRC Penji",
+    "url": "https://admin.penjiapp.com/login",
+    "category": "Quick Links",
+    "domain": "admin.penjiapp.com"
+  },
+  {
+    "title": "Beacon SI 2026-2027",
+    "url": "https://docs.google.com/spreadsheets/d/1b_NCZ33dM4Hb_R6yrrWQRuGSb3KDN-OH5eKwdM8zdVI/edit?gid=270916537#gid=270916537",
     "category": "Beacon",
     "domain": "docs.google.com"
   },
   {
     "title": "Beacon Attendance Tracking.docx - Google Docs",
     "url": "https://docs.google.com/document/d/1vWjIHJYS-HzrpcMayRNPinTKoOduCktF/edit",
-    "category": "Beacon",
+    "category": "Beacon / SI Leader Forms",
     "domain": "docs.google.com"
   },
   {
     "title": "No-Show Policy - Google Docs",
     "url": "https://docs.google.com/document/d/11Jnn9WA93-3bgstrZ48qdJb7MM_6XwhdfBx-VXoOHGA/edit?tab=t.0",
     "category": "Beacon",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "WRKS #1 (Jan-Feb)",
-    "url": "https://docs.google.com/spreadsheets/d/1WFPvROXwDTw_iCaqzYMrFk7KY6O9i2DA/edit?gid=494602454#gid=494602454",
-    "category": "Beacon / Worksheet/Timesheet",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "WRKS #2 (Feb-Mar)",
-    "url": "https://docs.google.com/spreadsheets/d/1sERVqOcVL_9h0K4JD9ZogZRuV_Rkohh4/edit?gid=621913726#gid=621913726",
-    "category": "Beacon / Worksheet/Timesheet",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "WRKS #3 (Mar-Apr)",
-    "url": "https://docs.google.com/spreadsheets/d/1474djVmLM9hJ4a4mYbg3jdEICyo9ikZ2/edit?gid=1438156962#gid=1438156962",
-    "category": "Beacon / Worksheet/Timesheet",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "WRKS #4 (Apr-May)",
-    "url": "https://docs.google.com/spreadsheets/d/1M9KrhbEk7R68OVqT6zwmniHNkNGvSH3T/edit?rtpof=true&sd=true&gid=1438156962#gid=1438156962",
-    "category": "Beacon / Worksheet/Timesheet",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "Preparing Timesheets - Google Docs",
-    "url": "https://docs.google.com/document/d/1bseUjMgtQ97zZ48xjN2Zs9L0Trs20JdVMlgSaw2i_EE/edit?tab=t.0",
-    "category": "Beacon / Worksheet/Timesheet",
     "domain": "docs.google.com"
   },
   {
@@ -312,15 +204,15 @@ window.BOOKMARKS = [
     "domain": "docs.google.com"
   },
   {
-    "title": "Hrly Formula (Fall & Spring)",
+    "title": "Hourly Formulas (Fall & Spring)",
     "url": "https://docs.google.com/document/d/1DMoJLbIoh1sFiw5xpcTr59EfSCIZjno21ArZ-uOUiHQ/edit?tab=t.0",
-    "category": "Beacon / Hrly Formulas",
+    "category": "Beacon / Hourly Formulas",
     "domain": "docs.google.com"
   },
   {
-    "title": "Hrly Formula (Summer)",
+    "title": "Hourly Formulas (Summer)",
     "url": "https://docs.google.com/document/d/1MVPyxBlKFIBGiHutvaOc2kGZ-l5UotTJ-Cwj_CfckQM/edit?tab=t.0",
-    "category": "Beacon / Hrly Formulas",
+    "category": "Beacon / Hourly Formulas",
     "domain": "docs.google.com"
   },
   {
@@ -354,12 +246,6 @@ window.BOOKMARKS = [
     "domain": "inside.arc.losrios.edu"
   },
   {
-    "title": "LRC",
-    "url": "https://arc.losrios.edu/lrc",
-    "category": "Beacon",
-    "domain": "arc.losrios.edu"
-  },
-  {
     "title": "Beacon",
     "url": "https://arc.losrios.edu/student-resources/campus-tutoring-programs/beacon-supplemental-instruction-(si)-program",
     "category": "Beacon",
@@ -378,24 +264,6 @@ window.BOOKMARKS = [
     "domain": "arc.losrios.edu"
   },
   {
-    "title": "Beacon Supplemental Instruction (SI) Program",
-    "url": "https://arc.losrios.edu/student-resources/campus-tutoring-programs/beacon-supplemental-instruction-%28si%29-program",
-    "category": "Beacon / Main Pages",
-    "domain": "arc.losrios.edu"
-  },
-  {
-    "title": "Beacon SI Leaders",
-    "url": "https://arc.losrios.edu/beacon-si-leaders",
-    "category": "Beacon / Main Pages",
-    "domain": "arc.losrios.edu"
-  },
-  {
-    "title": "Campus Tutoring Programs",
-    "url": "https://arc.losrios.edu/student-resources/campus-tutoring-programs",
-    "category": "Beacon / Main Pages",
-    "domain": "arc.losrios.edu"
-  },
-  {
     "title": "Find Beacon Sessions",
     "url": "https://calendar.google.com/calendar/u/0/embed?ctz=America%2FLos_Angeles&pli=1&src=c_3b98bccaaa60395fafb259d30072d7fa8d69c1503ed373402c4312f448f94594%40group.calendar.google.com",
     "category": "Beacon / Student Links",
@@ -408,31 +276,25 @@ window.BOOKMARKS = [
     "domain": "apps.arc.losrios.edu"
   },
   {
-    "title": "ARC WayFind",
-    "url": "https://wayfind.arc.losrios.edu/",
-    "category": "Beacon / Student Links",
-    "domain": "wayfind.arc.losrios.edu"
-  },
-  {
-    "title": "Cancel a Beacon Session",
+    "title": "Cancel Session",
     "url": "https://lrccd.formstack.com/forms/beacon_si_program_cancellation_form",
     "category": "Beacon / SI Leader Forms",
     "domain": "lrccd.formstack.com"
   },
   {
-    "title": "Beacon SI Group Request",
+    "title": "Group Request",
     "url": "https://lrccd.formstack.com/forms/beacon_group_request",
     "category": "Beacon / SI Leader Forms",
     "domain": "lrccd.formstack.com"
   },
   {
-    "title": "Beacon Planning Sheet",
+    "title": "Planning Sheet",
     "url": "https://docs.google.com/forms/d/e/1FAIpQLSe7MOLnAAVOKELGcyCJfftBWJ1K0pm6mo7ocaTCkjAunIMrRg/viewform?usp=send_form",
     "category": "Beacon / SI Leader Forms",
     "domain": "docs.google.com"
   },
   {
-    "title": "Midterm or Final Session Request",
+    "title": "Midterm/Final Session Request",
     "url": "https://lrccd.formstack.com/forms/beacon_si_program_midterm_final_session_request_form",
     "category": "Beacon / SI Leader Forms",
     "domain": "lrccd.formstack.com"
@@ -457,12 +319,6 @@ window.BOOKMARKS = [
   },
   {
     "title": "Service Central",
-    "url": "https://servicecentral.losrios.edu/auth_redirect.do?sysparm_stack=no&sysparm_url=https%3A%2F%2Flrccd.okta.com%2Fapp%2Flrccd_servicecentralmulti_1%2Fexkd93p0qmYlx4dgh697%2Fsso%2Fsaml%3FSAMLRequest%3DlVJbb9owFP4rkd9zwYR0WAQpA01D6rqosEndS%252BXYB7DqS%252BrjUPbvGwJTy8Mq9fX4O9%252F5Lp4hN5q2rOrC3t7DcwcYoqPRFtn5pSSdt8xxVMgsN4AsCLauftwymmSs9S444TSJKkTwQTm7cBY7A34N%252FqAE%252FLq%252FLck%252BhBZZmuJ5JsAGz3WiHXrlMAHZpZYfWr6DRDoSLXsRyvIT29uu9kLIxD0FnghnUt6259HjNanpdFCPoxSOT3I6brNn86CPudzti%252BlNiujSky0SfXNewGC6JFuuEUi0WpZkfbcYN9vJREzG2Zem2MqskDSnTTadQt7wTHLaA7HmiOoAb6uIHawsBm5DSWhGizgbxxndjG5YnjM6SWhe%252FCFRfYnrq7JS2d3H2TZnELLvm00d1z%252FXm4HgoCT4ux796Vh%252Fg8ch0p6dzGdDv2yQ7t9X%252FrEq%252Fq9nMr9u5iIitu7lVNAsfc9%252Fudayk%252FDVsnZaib9RpbV7WXjgoTcTfAdDL4aH%252F0sYJaNhomS8HaAMDFe6ktIDIknnl7vXP3r%252BCg%253D%253D%26RelayState%3Dhttps%253A%252F%252Fservicecentral.losrios.edu%252Fesc",
-    "category": "Rooms",
-    "domain": "servicecentral.losrios.edu"
-  },
-  {
-    "title": "Servicecentral",
     "url": "https://servicecentral.losrios.edu/esc",
     "category": "Quick Links",
     "domain": "servicecentral.losrios.edu"
@@ -516,7 +372,7 @@ window.BOOKMARKS = [
     "domain": "arc.losrios.edu"
   },
   {
-    "title": "Schedule Tutoring through Penji",
+    "title": "Schedule Tutoring Through Penji",
     "url": "https://web.penjiapp.com/",
     "category": "Scheduling & Student Tools",
     "domain": "web.penjiapp.com"
@@ -544,18 +400,6 @@ window.BOOKMARKS = [
     "url": "https://vimeo.com/1053084583/022858adc0?fl=pl&fe=cm",
     "category": "Scheduling & Student Tools",
     "domain": "vimeo.com"
-  },
-  {
-    "title": "Student Referral Form ? Faculty Use Only",
-    "url": "https://docs.google.com/forms/d/e/1FAIpQLSc-p6iMKZNI4lEEJOSlfscgIzPgyXNjCtGuhOQ-7rxBfWOa5w/viewform",
-    "category": "Forms",
-    "domain": "docs.google.com"
-  },
-  {
-    "title": "Share Your Tutoring Experience Form",
-    "url": "https://lrccd.formstack.com/forms/share_your_tutoring_experience",
-    "category": "Forms",
-    "domain": "lrccd.formstack.com"
   },
   {
     "title": "Learning Resource Center",
@@ -607,7 +451,7 @@ window.BOOKMARKS = [
   },
   {
     "title": "ARC Book Store",
-    "url": "https://www.bkstr.com/losriosstore/home",
+    "url": "https://losrios.bncollege.com/",
     "category": "ARC Book Store",
     "domain": "bkstr.com"
   },
@@ -619,7 +463,7 @@ window.BOOKMARKS = [
   },
   {
     "title": "Book Store Hours",
-    "url": "https://www.bkstr.com/losriosstore/store-hours",
+    "url": "https://losrios.bncollege.com/",
     "category": "Campus Hours",
     "domain": "bkstr.com"
   },
@@ -684,7 +528,7 @@ window.BOOKMARKS = [
     "domain": "arc.losrios.edu"
   },
   {
-    "title": "Student Center, near CLD",
+    "title": "Student Center, Near CLD",
     "url": "",
     "category": "Vending Machines",
     "domain": "Campus location",
@@ -698,38 +542,39 @@ window.BOOKMARKS = [
     "description": "4 beverage machines"
   },
   {
-    "title": "STEM first floor, near stairs",
+    "title": "STEM First Floor, Near Stairs",
     "url": "",
     "category": "Vending Machines",
     "domain": "Campus location",
     "description": "1 snack machine"
   },
   {
-    "title": "Administration, outside Counseling entrance",
+    "title": "Administration, Outside Counseling Entrance",
     "url": "",
     "category": "Vending Machines",
     "domain": "Campus location",
     "description": "2 beverage and 1 snack machine"
   },
   {
-    "title": "LRC, near entrance",
+    "title": "LRC, Near Entrance",
     "url": "",
     "category": "Vending Machines",
     "domain": "Campus location",
     "description": "1 beverage and 1 snack machine"
   },
   {
-    "title": "CTE, near Automotive",
+    "title": "CTE, Near Automotive",
     "url": "",
     "category": "Vending Machines",
     "domain": "Campus location",
     "description": "1 beverage and 1 snack machine"
   },
   {
-    "title": "Kinesiology, by Room 236",
+    "title": "Kinesiology, By Room 236",
     "url": "",
     "category": "Vending Machines",
     "domain": "Campus location",
     "description": "1 beverage and 1 snack machine"
   }
 ];
+
